f876cde7c2b588c0764d9bddefadf0a9380db3c6
guohan <poethan@foxmail.com>
2019-11-26 18:04:24 +0800

    addinfo

--- user/dao/picturedao.php
+++ user/dao/picturedao.php
@@ -2,6 +2,6 @@
 class PictureDao
 {
     public function getPictureOne($id){
-        return DB::get("user")->findOne("select * from picture where id = :id limit 1", [':id' => $id]);
+        return ['id' => $id, 'name' => 'gear'];
     }
 }
